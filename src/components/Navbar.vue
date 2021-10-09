cf5094c241dfdba1d9b205380e99975d86e5f4f1
Alexia <alexialsousa@hotmail.com>
2021-10-09 17:11:19 -0400

    Add simple navbar to top of every component

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,28 @@
 <template>
-  <div>
+<nav class="navbar navbar-expand-md">
+
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navBar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse navBar justify-content-center">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="/">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/Education">Education</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/Work">Work</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/Interests">Interests</a>
+      </li>
+    </ul>
   </div>
+
+</nav>
 </template>
 
 <script>
@@ -10,5 +32,13 @@ export default {
 </script>
 
 <style scoped>
-
+.navbar {
+    background-color: transparent, black !important;
+    
+}
+.nav-link{
+            color: black ;
+            font-size: 1.1em;
+            font-weight: bold;
+}
 </style>
