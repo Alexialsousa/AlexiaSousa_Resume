5acb4228624faf443ca2e6cb35e5b7f10d280fab
Alexia <alexialsousa@hotmail.com>
2021-10-09 17:10:11 -0400

    Add router links to all components

--- src/router/router.js
+++ src/router/router.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../components/Home'
+import Education from '../components/Education'
+import Work from '../components/Work'
+import Interests from '../components/Interests'
 
 Vue.use(VueRouter);
 
@@ -9,6 +12,21 @@ const routes = [
         name: 'Home',
         path: '/',
         component: Home
+    },
+    {
+        name: 'Education',
+        path: '/Education',
+        component: Education
+    },
+    {
+        name: 'Work',
+        path: '/Work',
+        component: Work
+    },
+    {
+        name: 'Interests',
+        path: '/Interests',
+        component: Interests
     },
 
 ];
