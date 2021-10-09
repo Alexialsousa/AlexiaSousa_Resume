987778c4b6f1b0ca76308cbb2aa65537b2914a18
Alexia <alexialsousa@hotmail.com>
2021-10-09 18:05:32 -0400

    Add vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,20 @@
 <template>
-  <div id="app">
-  <Navbar/>
-  <router-view/>
-  </div>
+  <v-app>
+    <v-main>
+      <Navbar/>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import Navbar from "./components/Navbar.vue";
+import Navbar from './components/Navbar';
 
 export default {
-  name: "App",
+  name: 'App',
+
   components: {
     Navbar,
-  }
+  },
 };
 </script>
-
-<style>
-
-</style>
