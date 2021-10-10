88c470cea4102f4501114c557e3fdde75caf963b
Alexia <alexialsousa@hotmail.com>
2021-10-10 01:31:01 -0400

    create home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,31 @@
 <template>
 <div>
+    <v-row justify="center" align-content="center">
+        <v-avatar class="justify-center ma-15" color="grey" size="184" rounded="160">
+            <img class="profile" src="../assets/profile.png" alt="avatar">
+        </v-avatar>
+    </v-row>
+    <v-container class="card">
+
+        <h1 style="display:inline">Alexia Sousa</h1>
+
+        <v-divider />
+        <h4 style="color: black">I'm a passionate Software Engineering Student at Concordia University
+            looking to gain knowledge and hands on experience in the programming world.
+        </h4>
+        <v-btn class="ma-5 button" width="25%" outlined href="AlexiaSousa_Resume.pdf" download>
+            Download Resume
+        </v-btn>
+    </v-container>
+
+        <v-row width="75%" justify="center">
+            <v-btn class="mx-2 ma-2" icon href="https://www.linkedin.com/in/alexiasousa/" target="_blank">
+                <v-icon size="30">fab fa-linkedin</v-icon>
+            </v-btn>
+            <v-btn class="mx-2 ma-2" icon href="https://github.com/alexialsousa" target="_blank">
+                <v-icon size="30">fab fa-github</v-icon>
+            </v-btn>
+        </v-row>
 
 </div>
 </template>
@@ -9,3 +35,21 @@ export default {
     name: 'Home',
 }
 </script>
+
+<style>
+.profile {
+    width: 19em;
+    height: 19em;
+}
+
+.card {
+    color: black !important;
+    text-align: center;
+    width: 100%;
+    display: inherit;
+    border: 0;
+    position: static;
+    margin-top: 0.5em;
+}
+
+</style>
